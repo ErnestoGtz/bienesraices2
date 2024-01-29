4299092f0556f3c445d87f90bd5705db29c70de4
Ernesto Gutierrez <ernesto.gtz@itcoach.com.mx>
2024-01-29 07:50:09 -0600

    HTML & Javascript finished

---
+++ base.php
@@ -0,0 +1,22 @@
+<?php
+  $inicio = false;
+  include 'includes/templates/header.php';
+?>
+    <main class="contenedor">
+      <h1>Titulo Pagina</h1>
+    </main>
+    <footer class="footer seccion">
+      <div class="contenedor contenerdor-footer">
+        <nav class="navegacion">
+          <a href="nosotros.html">Nosotros</a>
+          <a href="anuncios.html">Anuncios</a>
+          <a href="blog.html">Blog</a>
+          <a href="contacto.html">Contacto</a>
+        </nav>
+      </div>
+      <p class="copyright">Todos los derechos reservados 2024 &copy;</p>
+    </footer>
+    <script src="src/js/modernizr.js"></script>
+    <script src="build/js/bundle.min.js"></script>
+  </body>
+</html>
